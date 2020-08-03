098167d2ef9149d46c70533ceb8a16ed1663fe12
Hrvoje Lesar <hrvek@vmi423282.contaboserver.net>
2020-08-03 12:36:30 +0200

    changed cookie security

--- board/src/main.rs
+++ board/src/main.rs
@@ -93,7 +93,7 @@ async fn main() -> std::io::Result<()> {
                     .name("pojecme")
                     .login_deadline(time::Duration::hours(24))
                     .max_age(86400)
-                    .secure(false),
+                    .secure(true),
             ))
             .wrap(actix_web::middleware::Logger::default())
             .data(web::JsonConfig::default().limit(1024))
