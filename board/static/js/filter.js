05a6fff2ec88c0710c291e3ed44b6e56c84eb8c8
Hrvoje <hrvoje.lesar1@hotmail.com>
2020-08-26 16:34:46 +0200

    Search hotkey

--- board/static/js/filter.js
+++ board/static/js/filter.js
@@ -1,4 +1,20 @@
 $(function () {
+    $(".filter").attr("placeholder", "Click or press 'S' to search");
+
+    document.addEventListener("keydown", function(ev) {
+        if (document.activeElement.className === "filter") {
+            if (ev.code === "Escape" || ev.code === "Enter") {
+                $(".filter").blur();
+            }
+            return;
+        }
+
+        if (ev.code === "KeyS") {
+            ev.preventDefault();
+            $(".filter").focus();
+        }
+    });
+
     $(".filter").keyup(function() {
         let input = $(".filter").val().toLowerCase();
         let items = $(".grid-item");
