869c953a496eeb8d5ab56459cd7ec009693c10fc
Hrvoje <hrvoje.lesar1@hotmail.com>
2020-10-14 18:44:51 +0200

    When clicking on edit button shows name again

--- board/static/js/edit.js
+++ board/static/js/edit.js
@@ -10,5 +10,5 @@ $(".grid-container").on("show.bs.modal", ".editModal", function (event) {
     var display_name = button.data('display-name') // Extract info from data-* attributes
     var modal = $(this)
     modal.find('.modal-title').text('Editing ' + display_name)
-    modal.find('.modal-body #display_name').val(display_name)
+    modal.find('.modal-body .display_name').val(display_name)
 });
